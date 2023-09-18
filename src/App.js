722bf3adaa3af001b8ff93899d3d801fc2d43a8c
Vulpeshprajan <vulpeshprajan@gmail.com>
2023-09-18 10:03:39 +1000

    project started

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import SignIn from "./pages/signin-signup/SignIn";
+import SignUp from "./pages/signin-signup/SignUp";
+
+function App() {
+  return (
+    <div className="">
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+
+        {/* private router */}
+        <Route path="/new-admin" element={<SignUp />} />
+      </Routes>
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
